36e4ae04d70b57e9010f4320db28d5140ac0cd15
tyler36 <7234392+tyler36@users.noreply.github.com>
2024-10-22 12:59:35 +0900

    set media output

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,11 @@
 module.exports = {
+  video: true,
+  videosFolder: 'logs/video',
+  screenshotOnRunFailure: true,
+  screenshotsFolder: 'logs/screenshots',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-};
+}
